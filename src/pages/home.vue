aa0a79ea440471041488748825bd18151b7607fe
SQsuqi <643352633@qq.com>
2024-05-16 21:04:54 +0800

    init

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,15 +1,11 @@
 <template>
-  <div flex="~ items-center col" m-auto >
-    <div flex="~ items-center col">
-      <h1 text-3xl font-bold underline >
-        Hello Let's Go!
-      </h1>
-      <div i-carbon-arrow-down w-full p-y-10></div>
-      <button btn>
-        <router-link to="/hi">
-          <h1>Go to Router</h1>
-        </router-link>
-      </button>  
-    </div>
-  </div>
+    <h1 text-3xl font-bold underline >
+      Hello Let's Go!
+    </h1>
+    <div i-carbon-arrow-down w-full p-y-10></div>
+    <button btn>
+      <router-link to="/hi">
+        <h1>Go to Router</h1>
+      </router-link>
+    </button>  
 </template>
