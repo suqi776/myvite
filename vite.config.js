619f636c0b16b1f6aa4cc3d27578eb481acdbb93
sqsuqi <643352633@qq.com>
2024-11-27 17:14:48 +0800

    更新 `auto-import.d.ts` 文件，添加了 `// biome-ignore lint: disable` 注释并调整行末空行。重排 `vite.config.js` 文件中的导入顺序，将 `AutoImport` 和 `Components` 的导入位置进行了调整。

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite'
-import AutoImport from 'unplugin-auto-import/vite'
 import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
